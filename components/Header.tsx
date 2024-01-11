a98e9b234cbe51375d3cbcf77c2ff712d49abb42
dayongkr <dayongkr@gmail.com>
2024-01-11 12:37:43 +0900

    feat: add responsive style to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,16 +5,18 @@ import Nav from "./Nav";
 
 export default function Header() {
   return (
-    <header className="border-b-gray-200 border-b flex justify-start py-3 px-8 items-center gap-6">
-      <Link href="/" className="font-bold shrink-0">
-        Dayong Lee
-      </Link>
-      <Nav />
-      <Button variant="ghost" className="px-2">
-        <Link href="https://github.com/dayongkr" target="_blank">
-          <FaGithub className="text-lg" />
+    <header className="border-b-gray-200 border-b py-3 px-8 w-full flex justify-center items-center">
+      <div className="flex justify-start items-center gap-6 w-full max-w-7xl">
+        <Link href="/" className="font-bold shrink-0">
+          Dayong Lee
         </Link>
-      </Button>
+        <Nav />
+        <Button variant="ghost" className="px-2">
+          <Link href="https://github.com/dayongkr" target="_blank">
+            <FaGithub className="text-lg" />
+          </Link>
+        </Button>
+      </div>
     </header>
   );
 }
